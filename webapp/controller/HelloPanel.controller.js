9986cc833a93c4b26b7c239b2c67548a0617c1dd
Mehmet Akif Çolak <colakm.akif@gmail.com>
2026-03-30 01:08:07 +0300

    Initial commit: UI5 Walkthrough app

---
+++ webapp/controller/HelloPanel.controller.js
@@ -0,0 +1,39 @@
+sap.ui.define([
+  "sap/ui/core/mvc/Controller",
+  "sap/m/MessageToast",
+  "sap/ui/model/json/JSONModel"
+], (Controller, MessageToast, JSONModel) => {
+  "use strict";
+
+  return Controller.extend("ui5.walkthrough.controller.HelloPanel", {
+     onInit() {
+        const oModel = new JSONModel({ recipient: { name: "World" } });
+        this.getView().setModel(oModel);
+     },
+
+     onShowHello() {
+        // read msg from i18n model
+        const oBundle = this.getView().getModel("i18n").getResourceBundle();
+        const sRecipient = this.getView().getModel().getProperty("/recipient/name");
+        const sMsg = oBundle.getText("helloMsg", [sRecipient]);
+
+        // show message
+        MessageToast.show(sMsg);
+     },
+
+     async onOpenDialog() {
+         // create dialog lazily
+         this.oDialog ??= await this.loadFragment({
+             name: "ui5.walkthrough.view.HelloDialog"
+         });
+     
+         this.oDialog.open();
+     },
+
+     onCloseDialog() {
+       // note: We don't need to chain to the pDialog promise, since this event handler
+       // is only called from within the loaded dialog itself.
+       this.byId("helloDialog").close();
+     }
+  });
+});
