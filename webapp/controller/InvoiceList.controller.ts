801689a1028c910a66ce9ff15f01bda424da3a9a
Mehmet Akif Çolak <colakm.akif@gmail.com>
2026-03-31 01:07:00 +0300

    Finish Walkthrough Tutorial

--- webapp/controller/InvoiceList.controller.ts
+++ webapp/controller/InvoiceList.controller.ts
@@ -1,5 +1,14 @@
 import Controller from "sap/ui/core/mvc/Controller";
 import JSONModel from "sap/ui/model/json/JSONModel";
+import { SearchField$SearchEvent, SearchField$LiveChangeEvent } from "sap/m/SearchField";
+
+import Filter from "sap/ui/model/Filter";
+import FilterOperator from "sap/ui/model/FilterOperator";
+import ListBinding from "sap/ui/model/ListBinding";
+import UIComponent from "sap/ui/core/UIComponent";
+import Event from "sap/ui/base/Event";
+import ObjectListItem from "sap/m/ObjectListItem";
+import Table from "sap/m/Table";
 
 /**
  * @namespace ui5.walkthrough.controller
@@ -9,4 +18,29 @@ export default class InvoiceList extends Controller {
     const oViewModel = new JSONModel({ currency: "EUR" });
     this.getView()!.setModel(oViewModel, "view");
   }
+  onFilterInvoices(event: SearchField$SearchEvent): void {
+    this._filterList(event.getParameter("query") as string);
+  }
+
+  onLiveFilterInvoices(event: SearchField$LiveChangeEvent): void {
+    this._filterList(event.getParameter("newValue") as string);
+  }
+
+  private _filterList(sQuery: string): void {
+    const oTable = this.byId("invoiceList") as Table;
+    const oBinding = oTable.getBinding("items") as ListBinding;
+    const aFilters = sQuery
+      ? [new Filter("ProductName", FilterOperator.Contains, sQuery)]
+      : [];
+    oBinding.filter(aFilters);
+  }
+
+  onPress(event: Event): void {
+    const item = event.getSource() as ObjectListItem;
+
+    const router = UIComponent.getRouterFor(this);
+    router.navTo("detail", {
+        invoicePath: window.encodeURIComponent(item.getBindingContext("invoice")!.getPath().substring(1))
+    });
+  }     
 }
